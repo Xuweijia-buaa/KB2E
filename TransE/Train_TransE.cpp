d6961858deec82cc25d35df245fe1d9d64e77744
Xuweijia-buaa <xuwj@buaa.edu.cn>
2023-05-16 17:00:16 +0800

    first commit

--- TransE/Train_TransE.cpp
+++ TransE/Train_TransE.cpp
@@ -52,42 +52,47 @@ double vec_len(vector<double> &a)
 string version;
 char buf[100000],buf1[100000];
 int relation_num,entity_num;
-map<string,int> relation2id,entity2id;
+map<string,int> relation2id,entity2id;    // dict.全局map
 map<int,string> id2entity,id2relation;
 
 
-map<int,map<int,int> > left_entity,right_entity;
+map<int,map<int,int> > left_entity,right_entity; // left_entity[r]={h:count}   每种关系r,每个h的出现次数
 map<int,double> left_num,right_num;
 
 class Train{
 
 public:
-	map<pair<int,int>, map<int,int> > ok;
-    void add(int x,int y,int z)
+	map<pair<int,int>, map<int,int> > ok;  
+    void add(int x,int y,int z)            // 文件中解析得到的每个样本：h,t,r
     {
         fb_h.push_back(x);
         fb_r.push_back(z);
         fb_l.push_back(y);
-        ok[make_pair(x,z)][y]=1;
+        ok[make_pair(x,z)][y]=1;    // 查找<h,r>对应的[t]是否存在
     }
+
     void run(int n_in,double rate_in,double margin_in,int method_in)
     {
-        n = n_in;
+        n = n_in;                                            // 传入的k。
         rate = rate_in;
         margin = margin_in;
         method = method_in;
-        relation_vec.resize(relation_num);
+        relation_vec.resize(relation_num);          // 根据传入的size,resize模型参数vec
 		for (int i=0; i<relation_vec.size(); i++)
-			relation_vec[i].resize(n);
-        entity_vec.resize(entity_num);
+			relation_vec[i].resize(n);  
+
+        entity_vec.resize(entity_num);              // resize成|E|个。 每个resize成k
 		for (int i=0; i<entity_vec.size(); i++)
 			entity_vec[i].resize(n);
+
         relation_tmp.resize(relation_num);
 		for (int i=0; i<relation_tmp.size(); i++)
 			relation_tmp[i].resize(n);
         entity_tmp.resize(entity_num);
 		for (int i=0; i<entity_tmp.size(); i++)
 			entity_tmp[i].resize(n);
+
+        // 初始化
         for (int i=0; i<relation_num; i++)
         {
             for (int ii=0; ii<n; ii++)
@@ -105,14 +110,13 @@ public:
     }
 
 private:
-    int n,method;
-    double res;//loss function value
+    int n,method;  // k
+    double res;    // loss
     double count,count1;//loss function gradient
     double rate,margin;
     double belta;
-    vector<int> fb_h,fb_l,fb_r;
-    vector<vector<int> > feature;
-    vector<vector<double> > relation_vec,entity_vec;
+    vector<int> fb_h,fb_l,fb_r;                          // 所有样本：h,t,r (n_triple个)
+    vector<vector<double> > relation_vec,entity_vec;     // 模型参数. vec[id]=vector_embed
     vector<vector<double> > relation_tmp,entity_tmp;
     double norm(vector<double> &a)
     {
@@ -140,11 +144,11 @@ private:
             {
 
             	res=0;
-             	for (int batch = 0; batch<nbatches; batch++)
+             	for (int batch = 0; batch<nbatches; batch++)  // 每个batch
              	{
              		relation_tmp=relation_vec;
             		entity_tmp = entity_vec;
-             		for (int k=0; k<batchsize; k++)
+             		for (int k=0; k<batchsize; k++)           // 每次抽一个样本（和对应的负样本）。抽B次
              		{
 						int i=rand_max(fb_h.size());
 						int j=rand_max(entity_num);
@@ -153,17 +157,19 @@ private:
                             pr = 500;
 						if (rand()%1000<pr)
 						{
-							while (ok[make_pair(fb_h[i],fb_r[i])].count(j)>0)
+							while (ok[make_pair(fb_h[i],fb_r[i])].count(j)>0) // 抽一个存在的t
 								j=rand_max(entity_num);
+                            // 训练h[i],t[i].r[i], 和对应的负样本，t换了
 							train_kb(fb_h[i],fb_l[i],fb_r[i],fb_h[i],j,fb_r[i]);
 						}
 						else
 						{
 							while (ok[make_pair(j,fb_r[i])].count(fb_l[i])>0)
 								j=rand_max(entity_num);
+                            // 训练h[i],t[i].r[i], 和对应的负样本，h换了
 							train_kb(fb_h[i],fb_l[i],fb_r[i],j,fb_l[i],fb_r[i]);
 						}
-                		norm(relation_tmp[fb_r[i]]);
+                		norm(relation_tmp[fb_r[i]]); // 训练过的样本，涉及到的4个embedding，归一化
                 		norm(entity_tmp[fb_h[i]]);
                 		norm(entity_tmp[fb_l[i]]);
                 		norm(entity_tmp[j]);
@@ -172,18 +178,18 @@ private:
 		            entity_vec = entity_tmp;
              	}
                 cout<<"epoch:"<<epoch<<' '<<res<<endl;
-                FILE* f2 = fopen(("relation2vec."+version).c_str(),"w");
+                FILE* f2 = fopen(("relation2vec."+version).c_str(),"w"); // 每个epoch, checkpoint,写入最终的模型参数
                 FILE* f3 = fopen(("entity2vec."+version).c_str(),"w");
                 for (int i=0; i<relation_num; i++)
                 {
                     for (int ii=0; ii<n; ii++)
-                        fprintf(f2,"%.6lf\t",relation_vec[i][ii]);
+                        fprintf(f2,"%.6lf\t",relation_vec[i][ii]);    // r_embed: \t分割
                     fprintf(f2,"\n");
                 }
                 for (int i=0; i<entity_num; i++)
                 {
                     for (int ii=0; ii<n; ii++)
-                        fprintf(f3,"%.6lf\t",entity_vec[i][ii]);
+                        fprintf(f3,"%.6lf\t",entity_vec[i][ii]);      // e_embed: \t分割
                     fprintf(f3,"\n");
                 }
                 fclose(f2);
@@ -202,6 +208,7 @@ private:
             	sum+=sqr(entity_vec[e2][ii]-entity_vec[e1][ii]-relation_vec[rel][ii]);
         return sum;
     }
+
     void gradient(int e1_a,int e2_a,int rel_a,int e1_b,int e2_b,int rel_b)
     {
         for (int ii=0; ii<n; ii++)
@@ -213,7 +220,7 @@ private:
             		x=1;
             	else
             		x=-1;
-            relation_tmp[rel_a][ii]-=-1*rate*x;
+            relation_tmp[rel_a][ii]-=-1*rate*x;   // 梯度下降，修改模型参数
             entity_tmp[e1_a][ii]-=-1*rate*x;
             entity_tmp[e2_a][ii]+=-1*rate*x;
             x = 2*(entity_vec[e2_b][ii]-entity_vec[e1_b][ii]-relation_vec[rel_b][ii]);
@@ -229,12 +236,13 @@ private:
     }
     void train_kb(int e1_a,int e2_a,int rel_a,int e1_b,int e2_b,int rel_b)
     {
-        double sum1 = calc_sum(e1_a,e2_a,rel_a);
-        double sum2 = calc_sum(e1_b,e2_b,rel_b);
+        // 训练h[i],t[i].r[i], 和对应的负样本b
+        double sum1 = calc_sum(e1_a,e2_a,rel_a);  // 正样本a   h,t,r
+        double sum2 = calc_sum(e1_b,e2_b,rel_b);  // 负样本b   -h,-t,-r
         if (sum1+margin>sum2)
         {
         	res+=margin+sum1-sum2;
-        	gradient( e1_a, e2_a, rel_a, e1_b, e2_b, rel_b);
+        	gradient( e1_a, e2_a, rel_a, e1_b, e2_b, rel_b);// max_margin_loss 
         }
     }
 };
@@ -248,20 +256,21 @@ void prepare()
 	while (fscanf(f1,"%s%d",buf,&x)==2)
 	{
 		string st=buf;
-		entity2id[st]=x;
+		entity2id[st]=x;   // 全局变量 {name":id}
 		id2entity[x]=st;
 		entity_num++;
 	}
 	while (fscanf(f2,"%s%d",buf,&x)==2)
 	{
 		string st=buf;
-		relation2id[st]=x;
+		relation2id[st]=x;// 全局变量 {rname":rid}
 		id2relation[x]=st;
 		relation_num++;
 	}
     FILE* f_kb = fopen("../data/train.txt","r");
 	while (fscanf(f_kb,"%s",buf)==1)
     {
+        // (h,t),r，加入class中的数据中
         string s1=buf;
         fscanf(f_kb,"%s",buf);
         string s2=buf;
@@ -280,19 +289,19 @@ void prepare()
             relation2id[s3] = relation_num;
             relation_num++;
         }
-        left_entity[relation2id[s3]][entity2id[s1]]++;
+        left_entity[relation2id[s3]][entity2id[s1]]++;       // left_entity[r]={h:count}   每种关系r,每个h的出现次数
         right_entity[relation2id[s3]][entity2id[s2]]++;
-        train.add(entity2id[s1],entity2id[s2],relation2id[s3]);
+        train.add(entity2id[s1],entity2id[s2],relation2id[s3]);// 训练样本(h,t,r)
     }
     for (int i=0; i<relation_num; i++)
     {
     	double sum1=0,sum2=0;
-    	for (map<int,int>::iterator it = left_entity[i].begin(); it!=left_entity[i].end(); it++)
+    	for (map<int,int>::iterator it = left_entity[i].begin(); it!=left_entity[i].end(); it++)  // 遍历每个r的h
     	{
-    		sum1++;
-    		sum2+=it->second;
+    		sum1++;                    // 该r下，unique  (h,r)的数目
+    		sum2+=it->second;          // 该r下，       (h,r)的总数目
     	}
-    	left_num[i]=sum2/sum1;
+    	left_num[i]=sum2/sum1;         // 该r下，(h,r)重复率. 越大，越多重复(h,r)。说明该r下只有少数几种(h,r)，但出现多次
     }
     for (int i=0; i<relation_num; i++)
     {
